186a4c7132517519ef8163aa87cdae23d536e68e
AnthonyHarold67 <anthonyharold67@gmail.com>
2022-08-13 00:17:03 +0300

    navbar added

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("home", views.home, name="home"),
+    # path("", views.index, name="index"),
+    path("", views.home, name="home"),
     path("blogs", views.blogs, name="blogs"),
     path("blogDetail/<int:id>", views.blogDetail, name="blogDetail"),
 ]
